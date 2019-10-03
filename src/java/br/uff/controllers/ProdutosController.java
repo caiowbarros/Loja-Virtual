e999c5a27b1c4c2f5b846634094a64b23b216e0e
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2019-10-03 19:04:30 -0300

    redirect p pag d produtos

--- src/java/br/uff/controllers/ProdutosController.java
+++ src/java/br/uff/controllers/ProdutosController.java
@@ -30,19 +30,7 @@ public class ProdutosController extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProdutosController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProdutosController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        response.sendRedirect("produtos.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
