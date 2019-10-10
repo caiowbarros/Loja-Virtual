842d8baa0ed5c0108438af335c5a3e3885df9e20
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2019-10-09 23:07:44 -0300

    incluindo pacote simples de acesso ao bd

---
+++ src/java/br/uff/controllers/DaoExample.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.controllers;
+
+import br.uff.dao.MySql;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HP
+ */
+@WebServlet(name = "DaoExample", urlPatterns = {"/DaoExample"})
+public class DaoExample extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            MySql db = null;
+            try {
+                db = new MySql("test", "root", "");
+
+                String[] bind = {"2"};
+                String ret = db.dbValor("name", "roles", "id=?", bind);
+                out.println(ret);
+
+                out.println("<br>");
+
+                try {
+                    String[] bindGrava = {"TESTE"};
+                    db.dbGrava("INSERT INTO ROLES (NAME) VALUES (?)", bindGrava);
+                    out.println("gravacao ok");
+                } catch (SQLException e) {
+                    out.println("gravacao err");
+                }
+
+                ResultSet result = db.dbCarrega("SELECT * FROM ROLES", null);
+                while (result.next()) {
+                    out.println(result.getString("id"));
+                    out.println(result.getString("name"));
+                    out.println("<br>");
+                }
+
+            } catch (SQLException ex) {
+                out.println(ex.getMessage());
+            } finally {
+                db.destroyDb();
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DaoExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DaoExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/br/uff/dao/MySql.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HP
+ */
+public class MySql {
+
+    Connection conn = null;
+
+    public MySql(String dataBase, String nomeUsuario, String senhaUsuario) throws SQLException, ClassNotFoundException {
+        this.initDbMySql(dataBase, nomeUsuario, senhaUsuario);
+    }
+
+    public void dbTransaction(String[] comandos, String[][] bind) throws SQLException {
+        try {
+            this.conn.setAutoCommit(false);
+            Integer aux = 0;
+            for (String comando : comandos) {
+                // pega comando e verifica se index existe e pega bind
+                dbGrava(comando, aux < bind.length ? bind[aux] : null);
+                // incrementa aux
+                aux += 1;
+            }
+            this.conn.commit();
+        } catch (SQLException ex) {
+            this.conn.rollback();
+            throw new SQLException(ex.getMessage());
+        } finally {
+            this.conn.setAutoCommit(true);
+        }
+    }
+
+    public void dbGrava(String comando, String[] bind) throws SQLException {
+        PreparedStatement sql = montaComando(comando, bind);
+        sql.executeUpdate();
+        sql = fechaComando(sql);
+    }
+
+    public String dbValor(String campo, String tabela, String filtro, String[] bind) throws SQLException {
+        if (!tabela.toUpperCase().startsWith("SELECT")) {
+            tabela = "SELECT * FROM " + tabela;
+        }
+        if (!filtro.equals("")) {
+            tabela = "SELECT * FROM (" + tabela + ") _x1 WHERE " + filtro;
+        }
+        String consulta = "SELECT " + campo + " FROM (" + tabela + ") _x2";
+
+        PreparedStatement sql;
+        sql = montaComando(consulta, bind);
+        ResultSet resultado = sql.executeQuery();
+        if (resultado.next()) {
+            return resultado.getString(campo);
+        } else {
+            return null;
+        }
+    }
+
+    public ResultSet dbCarrega(String consulta, String[] bind) throws SQLException {
+        PreparedStatement sql = montaComando(consulta, bind);
+        ResultSet rs = sql.executeQuery();
+        return rs;
+    }
+
+    private void initDbMySql(String dataBase, String nomeUsuario, String senhaUsuario)
+            throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dataBase, nomeUsuario, senhaUsuario);
+    }
+
+    public void destroyDb() throws SQLException {
+        this.conn.close();
+    }
+
+    /**
+     *
+     * @param sql
+     * @param bind
+     * @return
+     * @throws java.sql.SQLException
+     */
+    public PreparedStatement montaComando(String sql, String[] bind) throws SQLException {
+        PreparedStatement comando;
+        // inicia comando
+        comando = this.conn.prepareStatement(sql);
+        // retorna comando com bind inserido
+        if (bind != null) {
+            comando = setBind(comando, bind);
+        }
+        return comando;
+    }
+
+    public PreparedStatement fechaComando(PreparedStatement comando) throws SQLException {
+        comando.close();
+        return comando;
+    }
+
+    public PreparedStatement setBind(PreparedStatement comando, String[] bind) throws SQLException {
+        // define contador
+        Integer contador = 1;
+        for (String valor : bind) {
+            // insere valor no Statement
+            comando.setString(contador, valor);
+            // incrementa contador
+            contador += 1;
+        }
+        // retorna comando com o bind definido
+        return comando;
+
+    }
+
+}
