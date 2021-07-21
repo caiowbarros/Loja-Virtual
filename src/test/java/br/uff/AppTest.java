8f077293f812d76e3fe4dc084630a73db948ddad
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-07-20 23:05:02 -0300

    Incluindo pasta para testes

---
+++ src/test/java/br/uff/AppTest.java
@@ -0,0 +1,12 @@
+package br.uff;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
