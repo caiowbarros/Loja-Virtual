f5c19fcb088cab3e86f3acbb4f2d4f15043dee6f
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-07-28 00:55:30 -0300

    Desenvolvendo bloco do endereco utilizando tdd

--- src/main/java/br/uff/loja/core/interfaces/data/IEnderecoData.java
+++ src/main/java/br/uff/loja/core/interfaces/data/IEnderecoData.java
@@ -1,10 +1,10 @@
-package br.uff.loja.core.interfaces.services;
+package br.uff.loja.core.interfaces.data;
 
 import java.util.List;
 
 import br.uff.loja.core.dtos.EnderecoDTO;
 
-public interface IEnderecoService {
+public interface IEnderecoData {
     public EnderecoDTO encontraEnderecoPorId(Integer id);
     public Integer excluiEnderecoPorId(Integer id);
     public Integer atualizaEnderecoPorId(Integer id, EnderecoDTO endereco);
--- src/test/java/br/uff/loja/LojaApplicationTests.java
+++ src/test/java/br/uff/loja/LojaApplicationTests.java
@@ -2,10 +2,14 @@ package br.uff.loja;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import br.uff.loja.core.dtos.AvaliacaoProdutoInsertDTO;
+import br.uff.loja.core.dtos.EnderecoDTO;
 import br.uff.loja.core.interfaces.services.IAvaliacaoService;
+import br.uff.loja.core.interfaces.services.IEnderecoService;
 import br.uff.loja.core.services.AvaliacaoService;
 
 public class LojaApplicationTests {
@@ -14,6 +18,7 @@ public class LojaApplicationTests {
         String exMessage = "";
         try {
             IAvaliacaoService avaliacaoService = new AvaliacaoService();
+
             AvaliacaoProdutoInsertDTO avaliacaoProdutoInsertDTO = new AvaliacaoProdutoInsertDTO();
             avaliacaoProdutoInsertDTO.usuarioId = 1;
             avaliacaoProdutoInsertDTO.descricao = "Testando descrição!";
@@ -27,5 +32,40 @@ public class LojaApplicationTests {
             exMessage = ex.getMessage();
         }
         assertEquals("O produto de id: 3 já foi avaliado!", exMessage);
+    }
+
+    public void testaInclusaoEndereco() {
+        IEnderecoService enderecoService = new EnderecoService();
+
+        EnderecoDTO endereco = new EnderecoDTO();
+        endereco.nome = "Casa";
+        endereco.logradouro = "Avenida Almirante Ary Parreiras, 6";
+        endereco.cep = "24230-322";
+        endereco.cidade = "Niterói";
+        endereco.estado = "RJ";
+        endereco.pais = "Brasil";
+        endereco.usuarioId = 1;
+
+        enderecoService.insereEndereco(endereco);
+    }
+
+    public void testaUpdateEndereco() {
+        IEnderecoService enderecoService = new EnderecoService();
+
+        List<EnderecoDTO> enderecosDoUsuario = enderecoService.listaEnderecosPorUsuarioId(1);
+        EnderecoDTO primeiroEndereco = enderecosDoUsuario.get(0);
+        primeiroEndereco.nome += "x";
+
+        enderecoService.atualizaEnderecoPorId(primeiroEndereco.id, primeiroEndereco);
+    }
+
+    public void testaExclusaoEndereco() {
+        IEnderecoService enderecoService = new EnderecoService();
+        List<EnderecoDTO> enderecosDoUsuario = enderecoService.listaEnderecosPorUsuarioId(1);
+        EnderecoDTO primeiroEndereco = enderecosDoUsuario.get(0);
+
+        enderecoService.excluiEnderecoPorId(primeiroEndereco.id);
+
+        enderecoService.encontraEnderecoPorId(primeiroEndereco.id);
     }
 }
