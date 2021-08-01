2940fa5be0516771d217b4844bad5419515fcb12
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-08-01 16:07:16 -0300

    Incluindo metodo p listar usuarios

--- src/main/java/br/uff/loja/core/interfaces/data/IUsuarioData.java
+++ src/main/java/br/uff/loja/core/interfaces/data/IUsuarioData.java
@@ -1,10 +1,13 @@
 package br.uff.loja.core.interfaces.data;
 
+import java.util.List;
+
 import br.uff.loja.core.dtos.UsuarioDTO;
 import br.uff.loja.core.exceptions.LojaException;
 
 public interface IUsuarioData {
     public UsuarioDTO encontraUsuarioPorId(Integer id) throws LojaException;
+    public List<UsuarioDTO> listaUsuarios() throws LojaException;
     public void atualizaUsuarioPorId(Integer id, UsuarioDTO usuario) throws LojaException;
     public Integer encontraUsuarioIdPorEmailESenha(String email, String senha) throws LojaException;
     public void insereUsuario(UsuarioDTO usuario) throws LojaException;
--- src/main/java/br/uff/loja/core/interfaces/services/IUsuarioService.java
+++ src/main/java/br/uff/loja/core/interfaces/services/IUsuarioService.java
@@ -1,9 +1,12 @@
 package br.uff.loja.core.interfaces.services;
 
+import java.util.List;
+
 import br.uff.loja.core.dtos.UsuarioDTO;
 import br.uff.loja.core.exceptions.LojaException;
 
 public interface IUsuarioService {
     public UsuarioDTO login(String email, String senha) throws LojaException;
     public UsuarioDTO gravaUsuario(UsuarioDTO usuario) throws LojaException;
+    public List<UsuarioDTO> listaUsuarios() throws LojaException;
 }
