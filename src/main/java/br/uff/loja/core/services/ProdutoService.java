87a2722687ce1b37f7ae380c188c7f6e2a0cb251
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-08-01 03:09:19 -0300

    Finalizando ProdutosService

--- src/main/java/br/uff/loja/core/services/ProdutoService.java
+++ src/main/java/br/uff/loja/core/services/ProdutoService.java
@@ -2,67 +2,70 @@ package br.uff.loja.core.services;
 
 import java.util.List;
 
+import br.uff.loja.core.dtos.FiltraProdutoDTO;
 import br.uff.loja.core.dtos.PaginateDTO;
 import br.uff.loja.core.dtos.ProdutoDTO;
 import br.uff.loja.core.dtos.ProdutoListaDTO;
 import br.uff.loja.core.dtos.ProdutoVitrineUsuarioDTO;
 import br.uff.loja.core.exceptions.LojaException;
+import br.uff.loja.core.interfaces.data.IProdutoData;
 import br.uff.loja.core.interfaces.services.IProdutoService;
+import br.uff.loja.infrastructure.data.ProdutoData;
 
 public class ProdutoService implements IProdutoService {
+    private IProdutoData produtoData;
+
+    public ProdutoService() {
+        produtoData = new ProdutoData();
+    }
 
     @Override
     public ProdutoDTO encontraProdutoPorId(Integer id) throws LojaException {
-        // TODO Auto-generated method stub
-        return null;
+        return produtoData.encontraProdutoPorId(id);
     }
 
     @Override
     public void excluiProdutoPorId(Integer id) throws LojaException {
-        // TODO Auto-generated method stub
-        
+        produtoData.excluiProdutoPorId(id);        
     }
 
     @Override
     public ProdutoDTO atualizaProdutoPorId(Integer id, ProdutoDTO produto) throws LojaException {
-        // TODO Auto-generated method stub
-        return null;
+        produtoData.atualizaProdutoPorId(id, produto);
+        return produtoData.encontraProdutoPorId(id);
     }
 
     @Override
     public void insereQuantidadeEmEstoqueDoProdutoPorId(Integer id, Integer quantidade) throws LojaException {
-        // TODO Auto-generated method stub
-        
+        produtoData.insereQuantidadeEmEstoqueDoProdutoPorId(id, quantidade);
     }
 
     @Override
     public void insereProduto(ProdutoDTO produto) throws LojaException {
-        // TODO Auto-generated method stub
-        
+        produtoData.insereProduto(produto);
     }
 
     @Override
     public void usuarioToogleFavoritaProdutoPorId(Integer produtoId, Integer usuarioId) throws LojaException {
-        // TODO Auto-generated method stub
-        
+        if(Boolean.TRUE.equals(produtoData.produtoFavoritadoPeloUsuario(produtoId, usuarioId))) {
+            produtoData.removeFavoritacaoProdutoPeloUsuario(produtoId, usuarioId);
+        } else {
+            produtoData.adicionaFavoritacaoProdutoPeloUsuario(produtoId, usuarioId);
+        }
     }
 
     @Override
     public List<ProdutoDTO> listaProdutosAdm() throws LojaException {
-        // TODO Auto-generated method stub
-        return null;
+        return produtoData.listaProdutosAdm();
     }
 
     @Override
     public PaginateDTO<List<ProdutoListaDTO>> listaProdutosVitrine(Integer pagina) throws LojaException {
-        // TODO Auto-generated method stub
-        return null;
+        return produtoData.listaProdutosVitrine(new FiltraProdutoDTO());
     }
 
     @Override
-    public ProdutoVitrineUsuarioDTO mostraProdutoVitrineParaUsuario(Integer id, Integer usuarioId)
-            throws LojaException {
-        // TODO Auto-generated method stub
-        return null;
+    public ProdutoVitrineUsuarioDTO mostraProdutoVitrineParaUsuario(Integer id, Integer usuarioId) throws LojaException {
+        return produtoData.mostraProdutoVitrineParaUsuario(id, usuarioId);
     }
 }
