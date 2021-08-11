4edbcf580b1bed0d52c8b776505c029634cbeb5d
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-08-11 02:29:53 -0300

    Ajuste na estrutura pra comecar a preparar os controllers e views

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/endorsed/webapp-runner.jar --port $PORT target/loja-1.0-SNAPSHOT.war
---
+++ src/main/java/br/uff/loja/application/components/SelectBean.java
@@ -0,0 +1,9 @@
+package br.uff.loja.application.components;
+
+import java.io.Serializable;
+
+public class SelectBean implements Serializable {
+    private static final long serialVersionUID = -8266471152053172467L;
+
+    public SelectBean() {}
+}
