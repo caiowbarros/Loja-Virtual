5a218f3aa9d36ec711b4ce135c8613a6e032f25b
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-08-28 16:06:32 -0300

    Ajustando instanciacao do produto dto e o helper q mostra preco das coisas

--- src/main/java/br/uff/loja/core/dtos/ProdutoDTO.java
+++ src/main/java/br/uff/loja/core/dtos/ProdutoDTO.java
@@ -78,35 +78,23 @@ public class ProdutoDTO extends BaseDTO {
     }
 
     public ProdutoDTO() {
+        this.setNome("");
+        this.setPreco(0.0);
+        this.setDescricao("");
     }
 
     public ProdutoDTO(Map<String, Object> produto) {
         this.setId(Integer.valueOf(String.valueOf(produto.get("id"))));
-        if (produto.get("nome") != null) {
-            this.setNome(String.valueOf(produto.get("nome")));
-        }
-        if (produto.get("preco") != null) {
-            this.setPreco(Double.valueOf(String.valueOf(produto.get("preco"))));
-        }
-        if (produto.get("descricao") != null) {
-            this.setDescricao(String.valueOf(produto.get("descricao")));
-        }
-        if (produto.get("imagem") != null) {
-            this.setImagem(String.valueOf(produto.get("imagem")));
-        }
-        if (produto.get("categoria") != null) {
-            this.setCategoria(String.valueOf(produto.get("categoria")));
-        }
-        if (produto.get("categoriaId") != null) {
-            this.setCategoriaId(Integer.valueOf(String.valueOf(produto.get("categoriaId"))));
-        }
-        if (produto.get("quantidade") != null) {
-            this.setQuantidade(Integer.valueOf(String.valueOf(produto.get("quantidade"))));
-        }
+        this.setNome(String.valueOf(produto.get("nome")));
+        this.setPreco(Double.valueOf(String.valueOf(produto.get("preco"))));
+        this.setDescricao(String.valueOf(produto.get("descricao")));
+        this.setImagem(String.valueOf(produto.get("imagem")));
+        this.setCategoria(String.valueOf(produto.get("categoria")));
+        this.setCategoriaId(Integer.valueOf(String.valueOf(produto.get("categoriaId"))));
+        this.setQuantidade(Integer.valueOf(String.valueOf(produto.get("quantidade"))));
     }
 
-    public ProdutoDTO(Integer id, String nome, Double preco, String descricao, String imagem, Integer categoriaId,
-            Integer quantidade) {
+    public ProdutoDTO(Integer id, String nome, Double preco, String descricao, String imagem, Integer categoriaId, Integer quantidade) {
         this.setId(id);
         this.setNome(nome);
         this.setPreco(preco);
